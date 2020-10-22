a646666bc1aa683cd73797d76d0951fe151b76c4
JanAlexanderZak <janalexzak@gmail.com>
2020-10-22 13:26:03 +0200

    Igrnore mypy and pytest folder on upload

---
+++ script.py
@@ -0,0 +1,25 @@
+""" Replaces script.bat """
+
+import subprocess
+import os
+
+
+def main():
+    # Executes commands
+    #x = os.system("pytest --cov=tests --cov-report=html:tests/pytest tests")
+    print('#' * 80)
+    #os.system("mypy --config-file=src/mypy.ini src/ --html-report tests/mypy")
+    print('#' * 80)
+    # os.system("pylint --rcfile=src/.pylintrc src/")
+    print('#' * 80)
+
+    # Trying to catch the output
+    # subprocess.run(["cmd", "dir"], shell=True)
+
+
+if __name__ == "__main__":
+    main()
+
+
+rmdir /s tests\mypy\html
+del tests\pytest\*.png
--- tests/script.bat
+++ tests/script.bat
@@ -1,7 +1,5 @@
-#!/bin/sh
+#!/bin/bat
 
 pytest --cov=tests --cov-report=html:tests/pytest tests
 mypy --config-file=src/mypy.ini src/ --html-report tests/mypy
 pylint --rcfile=src/.pylintrc src/
-
-cmd /k
